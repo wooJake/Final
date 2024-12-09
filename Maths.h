bff73d74aca2edfca569c45c14a3042add2e8d75
wooJake <167344799+wooJake@users.noreply.github.com>
2024-12-08 21:03:53 -0800

    Create Maths.h

---
+++ Maths.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>//Jake
+
+class Maths {
+
+public:
+
+	unsigned int width = 1280;//rylee
+	unsigned int height = 720;//rylee
+
+	const float pi = 3.14159265f;//Jake
+	const float tau = 2.0f * pi;//Jake
+	const float degree = tau / 360.0f;//Jake
+};
