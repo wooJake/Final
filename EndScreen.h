25342bb48fc4eabf69e8ef717e21f670cc997ebd
wooJake <167344799+wooJake@users.noreply.github.com>
2024-12-08 21:04:29 -0800

    Create EndScreen.h

---
+++ EndScreen.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <SFML/Graphics.hpp>//Jake
+#include <SFML/System.hpp>//Jake
+
+#include "Maths.h"
+
+class EndScreen {
+
+private:
+
+	Maths maths;
+
+	sf::Font font;//Jake
+
+	sf::Text endText;//Jake
+
+	void InitFont();
+	void InitEndText();
+
+public:
+
+	EndScreen();
+	~EndScreen();
+
+	void Render(sf::RenderWindow& target);
+};
