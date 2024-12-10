fa485de554ac9b95624d8391f7f9949d8c7080a7
wooJake <167344799+wooJake@users.noreply.github.com>
2024-12-09 17:20:19 -0800

    Update Ship.h
    Implemented flame stuffs

--- Ship.h
+++ Ship.h
@@ -1,4 +1,3 @@
-#pragma once
 #include "Maths.h"
 #include "Health.h"
 
@@ -16,16 +15,20 @@ private:
 	sf::RectangleShape leftThruster;//Jake
 	sf::RectangleShape rightThruster;//Jake
 	sf::RectangleShape leftFlame;//Jake
-	sf::RectangleShape righFlame;//Jake
+	sf::RectangleShape rightFlame;//Jake
 
 	float shipRadius = 27.0f;//Jake
 	sf::Vector2f shipVel = sf::Vector2f(0.0f, 0.0f);//rylee
 	float shipAccel = 1.03f;//rylee
 	float shipRotAccel = 3.00f;//Jake
 
+	bool leftFiring;//Jake
+	bool rightFiring;//Jake
+
 	void InitShip();//Jake
 
 	void MoveShip(float friction);//Jake
+	void MoveThrusters();//Jake
 	void WrapWorld();//Jake
 
 public:
@@ -34,9 +37,13 @@ public:
 	~Ship();//Jake
 
 	void Update(float friction);//Jake
-	void Render(sf::RenderWindow& target);//Jake
 
 	sf::CircleShape GetShip();//Jake
 	sf::RectangleShape GetLeftThruster();//Jake
 	sf::RectangleShape GetRightThruster();//Jake
+	sf::RectangleShape GetLeftFlame();//Jake
+	sf::RectangleShape GetRightFlame();//Jake
+
+	bool GetLeftFiring();//Jake
+	bool GetRightFiring();//Jake
 };
